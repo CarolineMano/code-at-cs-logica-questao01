046b8b3f73d96d3fdbbf6955e2f8600ce8d97b57
CarolineMano <carolinemano@protonmail.ch>
2022-12-18 17:48:31 -0300

    Questão 03

--- Program.cs
+++ Program.cs
@@ -1,33 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace Questao01
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             int distanciaPercorrida = 0;
             string distanciasIndividuais = "";
-            
             string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var percurso = new List<int>();
+            var tabelaDistancias = new int[5, 5];
 
-            var tabelaDistancias = File.ReadAllLines($"{desktop}\\matriz.txt")
-                                    .Select(l => l.Split(',').Select(i => int.Parse(i)).ToArray())
-                                    .ToArray();
+            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+                Delimiter = ","
+            };
 
-            var percurso = File.ReadAllLines($"{desktop}\\caminho.txt")[0].Split(",")
-                            .Select(i => int.Parse(i)).ToArray();
+            using (var leitorMatriz = new StreamReader($"{desktop}\\matriz.txt"))
+            using (var csv = new CsvReader(leitorMatriz, config))
+            {
+                var linha = 0;
+                var coluna = 0;
+                while (csv.Read())
+                {
+                    for (int i = 0; csv.TryGetField<int>(i, out var cidade); i++)
+                    {
+                        coluna = i % 5;
+                        tabelaDistancias[linha, coluna] = cidade;                            
+                    }
+                    linha++;
+                }
+            }
+
+            using (var leitorCaminho = new StreamReader($"{desktop}\\caminho.txt"))
+            using (var csv = new CsvReader(leitorCaminho, config))
+            {
+                while (csv.Read())
+                {
+                    for (int i = 0; csv.TryGetField<int>(i, out var cidade); i++)
+                    {
+                        percurso.Add(cidade);
+                    }
+                }
+            }
 
             Console.Write($"A distância percorrida foi de: ");
 
-            for (int i = 0; i < percurso.Length - 1; i++)
+            for (int i = 0; i < percurso.Count - 1; i++)
             {
                 var cidadeAtual = percurso[i] - 1;
-                var proximaCidade = percurso[i+1] - 1;
-                distanciaPercorrida += tabelaDistancias[cidadeAtual][proximaCidade];
-                distanciasIndividuais += $" {tabelaDistancias[cidadeAtual][proximaCidade]} +";
+                var proximaCidade = percurso[i + 1] - 1;
+                distanciaPercorrida += tabelaDistancias[cidadeAtual, proximaCidade];
+                distanciasIndividuais += $" {tabelaDistancias[cidadeAtual, proximaCidade]} +";
             }
 
             Console.Write($"{distanciasIndividuais.Substring(0, distanciasIndividuais.Length - 1)}= {distanciaPercorrida} Km");
--- README.md
+++ README.md
@@ -1,25 +1,3 @@
-# Questão #2
+# Questão #3
 
-Modifique o exercício anterior para ler a matriz e o caminho percorrido de arquivos.
-
-O nome do arquivo com os dados da matriz é matriz.txt e o nome do arquivo com os dados do caminho percorrido é caminho.txt.
-
-Veja um exemplo desses arquivos.
-
-**matriz.txt**
-
-```
-00,15,30,05,12
-15,00,10,17,28
-30,10,00,03,11
-05,17,03,00,80
-12,28,11,80,00 
-```
-
-**caminho.txt**
-
-```
-1, 2, 3, 2, 5, 1, 4
-```
-
-*Obs: Os arquivos devem dicar na área de trabalho.*
+Modifique o exercício 2 para utilizar a biblioteca CsvHelper ao fazer a leitura do arquivo.
